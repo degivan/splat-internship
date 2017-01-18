c30ff3a861eefe1f3506d49413f4e9fec896cd65
sayfutdinovig <i.g.sayfutdinov@gmail.com>
2017-01-19 00:51:46 +0300

    2 Full Services

--- CorePunterService/src/main/java/ru/splat/repository/ExactlyOnceRepositoryInterface.java
+++
@@ -1,8 +0,0 @@
-package ru.splat.repository;
-
-import java.util.List;
-
-public interface ExactlyOnceRepositoryInterface<V> {
-    void insertFilterTable(List<V> transactionResults);
-    List<V> filterByTable(List<Long> transactionIds);
-}
