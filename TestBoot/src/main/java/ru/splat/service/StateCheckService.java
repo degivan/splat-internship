e00a8c5018cffd5af5ba8636108a529a471056d1
sayfutdinovig <i.g.sayfutdinov@gmail.com>
2017-03-02 21:02:45 +0300

    KafkaImpl

--- TestBoot/src/main/java/ru/splat/service/StateCheckService.java
+++ TestBoot/src/main/java/ru/splat/service/StateCheckService.java
@@ -1,38 +1,38 @@
-package ru.splat.service;
-
-import com.google.gson.Gson;
-import ru.splat.messages.proxyup.bet.NewResponse;
-import ru.splat.messages.proxyup.bet.NewResponseClone;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
-/**
- * Created by Дмитрий on 10.02.2017.
- */
-public class StateCheckService {
-    private static final String URL_ADRESS = "http://localhost:8080/checkbet?transactionId=";   //заглушка, узнать и Ивана форму запроса стейта
-
-    public int makeRequest(NewResponseClone trdata) throws Exception
-    {
-        Gson g = new Gson();
-        URL url = new URL(URL_ADRESS + trdata.getTransactionId() + "&userId=" + trdata.getUserId());
-        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-        connection.setRequestMethod("GET");
-       // connection.setRequestProperty("Content-Type", "application/json");
-
-        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String inputLine;
-        StringBuilder response = new StringBuilder();
-        while((null != (inputLine = in.readLine())))
-        {
-            response.append(inputLine);
-        }
-        in.close();
-        int betState = g.fromJson(response.toString(), Integer.class);
-        return betState;
-    }
-
-}
+//package ru.splat.service;
+//
+//import com.google.gson.Gson;
+//import ru.splat.messages.proxyup.bet.NewResponse;
+//import ru.splat.messages.proxyup.bet.NewResponseClone;
+//
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//import java.net.HttpURLConnection;
+//import java.net.URL;
+//
+///**
+// * Created by Дмитрий on 10.02.2017.
+// */
+//public class StateCheckService {
+//    private static final String URL_ADRESS = "http://localhost:8080/checkbet?transactionId=";   //заглушка, узнать и Ивана форму запроса стейта
+//
+//    public int makeRequest(NewResponseClone trdata) throws Exception
+//    {
+//        Gson g = new Gson();
+//        URL url = new URL(URL_ADRESS + trdata.getTransactionId() + "&userId=" + trdata.getUserId());
+//        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+//        connection.setRequestMethod("GET");
+//       // connection.setRequestProperty("Content-Type", "application/json");
+//
+//        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+//        String inputLine;
+//        StringBuilder response = new StringBuilder();
+//        while((null != (inputLine = in.readLine())))
+//        {
+//            response.append(inputLine);
+//        }
+//        in.close();
+//        int betState = g.fromJson(response.toString(), Integer.class);
+//        return betState;
+//    }
+//
+//}
--- TestBoot/src/main/java/ru/splat/task/RequestTask.java
+++ TestBoot/src/main/java/ru/splat/task/RequestTask.java
@@ -1,72 +1,72 @@
-package ru.splat.task;
-
-
-import org.apache.log4j.Logger;
-import ru.splat.messages.proxyup.bet.NewResponse;
-import ru.splat.messages.proxyup.bet.NewResponseClone;
-import ru.splat.service.BootService;
-import java.util.concurrent.ConcurrentSkipListSet;
-
-
-
-public class RequestTask implements Runnable {
-
-    private int requestCount;
-    private long requestTimeout;
-    private int punterCount;
-    private ConcurrentSkipListSet<NewResponseClone> trIdSet;
-    private static Logger LOGGER = Logger.getLogger(RequestTask.class);
-
-    public RequestTask(int requestCount, long requestTimeout, int punterCount, ConcurrentSkipListSet<NewResponseClone> trIdSet) {
-        this.requestCount = requestCount;
-        this.requestTimeout = requestTimeout;
-        this.punterCount = punterCount;
-        this.trIdSet = trIdSet;
-    }
-
-    @Override
-    public void run() {
-
-        BootService bootService = new BootService();
-
-        while (!Thread.currentThread().interrupted())
-        {
-            int i=0;
-            long timeStart = System.currentTimeMillis();
-            long residual = 0;
-            while (i < requestCount && residual < requestTimeout)
-            {
-                try {
-                    NewResponseClone newResponse = bootService.makeRequest(punterCount);
-                    LOGGER.info("Response from server: " + newResponse.toString());
-                    //System.out.println(newResponse.getActive());
-                    if (!newResponse.getActive()) trIdSet.add(newResponse);  //добавление нового id в сет
-                    else LOGGER.info("Another transaction is active for userId = " + newResponse.getUserId());
-                }catch (InterruptedException ie)
-                {
-                    Thread.currentThread().interrupt();
-                    i = requestCount;
-                }
-                catch (Exception e) {
-                    LOGGER.error("High level",e);
-                    i = requestCount;
-                }
-
-                residual = System.currentTimeMillis() - timeStart;
-                i++;
-            }
-
-
-            if (residual < requestTimeout)
-            {
-                long freeTime = requestTimeout - residual;
-                LOGGER.info("Sleep time: " + freeTime);
-                try {
-                    Thread.currentThread().sleep(freeTime);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
-            }
-        }
-    }
-}
+//package ru.splat.task;
+//
+//
+//import org.apache.log4j.Logger;
+//import ru.splat.messages.proxyup.bet.NewResponse;
+//import ru.splat.messages.proxyup.bet.NewResponseClone;
+//import ru.splat.service.BootService;
+//import java.util.concurrent.ConcurrentSkipListSet;
+//
+//
+//
+//public class RequestTask implements Runnable {
+//
+//    private int requestCount;
+//    private long requestTimeout;
+//    private int punterCount;
+//    private ConcurrentSkipListSet<NewResponseClone> trIdSet;
+//    private static Logger LOGGER = Logger.getLogger(RequestTask.class);
+//
+//    public RequestTask(int requestCount, long requestTimeout, int punterCount, ConcurrentSkipListSet<NewResponseClone> trIdSet) {
+//        this.requestCount = requestCount;
+//        this.requestTimeout = requestTimeout;
+//        this.punterCount = punterCount;
+//        this.trIdSet = trIdSet;
+//    }
+//
+//    @Override
+//    public void run() {
+//
+//        BootService bootService = new BootService();
+//
+//        while (!Thread.currentThread().interrupted())
+//        {
+//            int i=0;
+//            long timeStart = System.currentTimeMillis();
+//            long residual = 0;
+//            while (i < requestCount && residual < requestTimeout)
+//            {
+//                try {
+//                    NewResponseClone newResponse = bootService.makeRequest(punterCount);
+//                    LOGGER.info("Response from server: " + newResponse.toString());
+//                    //System.out.println(newResponse.getActive());
+//                    if (!newResponse.getActive()) trIdSet.add(newResponse);  //добавление нового id в сет
+//                    else LOGGER.info("Another transaction is active for userId = " + newResponse.getUserId());
+//                }catch (InterruptedException ie)
+//                {
+//                    Thread.currentThread().interrupt();
+//                    i = requestCount;
+//                }
+//                catch (Exception e) {
+//                    LOGGER.error("High level",e);
+//                    i = requestCount;
+//                }
+//
+//                residual = System.currentTimeMillis() - timeStart;
+//                i++;
+//            }
+//
+//
+//            if (residual < requestTimeout)
+//            {
+//                long freeTime = requestTimeout - residual;
+//                LOGGER.info("Sleep time: " + freeTime);
+//                try {
+//                    Thread.currentThread().sleep(freeTime);
+//                } catch (InterruptedException e) {
+//                    Thread.currentThread().interrupt();
+//                }
+//            }
+//        }
+//    }
+//}
